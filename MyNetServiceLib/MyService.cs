a6c3ed1b1aaf04e183cf64f6b018475ee2d949c4
Harry Douglass <harry.douglass@gmail.com>
2020-11-01 23:03:55 -0500

    Add project files.

---
+++ MyNetServiceLib/MyService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using Microsoft.Extensions.Logging;
+
+namespace MyNetServiceLib
+{
+    public class MyService
+    {
+        private readonly ILogger<MyService> _logger;
+
+        public MyService(ILogger<MyService> logger = null)
+        {
+            _logger = logger;
+        }
+
+        public void DoSomething(string theParam)
+        {
+            _logger?.LogInformation($"DoSomething with {theParam}");
+
+            Thread.Sleep(1000);
+
+            // Test logs at each level
+            _logger?.LogTrace("DoSometing TRACE message");
+            _logger?.LogDebug("DoSometing DEBUG message");
+            _logger?.LogInformation("DoSometing INFO message");
+            _logger?.LogWarning("DoSometing WARN message");
+            _logger?.LogError("DoSometing ERROR message");
+            _logger?.LogCritical("DoSometing CRITICAL message");
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from console app.");
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Get an instance of the service library
+            var myService = serviceProvider.GetService<MyNetServiceLib.MyService>();
+
+            // Call the service, logs are made here
+            myService.DoSomething("My param passed in");
+
+            Console.ReadLine();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(config => { config.AddSerilog(); })
+                .AddTransient<MyNetServiceLib.MyService>();
+        }
+    }
+}
